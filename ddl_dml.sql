924f68bd84ad408af7ac8d06dc2b1ee67d0790da
trisna14 <151597595+trisna14@users.noreply.github.com>
2023-11-22 11:40:52 +0700

    Add files via upload

---
+++ ddl_dml.sql
@@ -0,0 +1,27 @@
+CREATE TABLE table_project (
+    "Invoice ID" text,
+    "Branch" text,
+    "City" text,
+    "Customer type" text,
+    "Gender" text,
+    "Product line" text,
+    "Unit price" double precision,
+    "Quantity" integer,
+    "Tax 5%" double precision,
+    "Total" double precision,
+    "Date" date,
+    "Time" time without time zone,
+    "Payment" text,
+    "cogs" double precision,
+    "gross margin percentage" double precision,
+    "gross income" double precision,
+    "Rating" double precision
+);
+
+COPY table_project("Invoice ID", "Branch", "City", "Customer type", "Gender", "Product line", "Unit price", "Quantity", "Tax 5%", "Total", "Date", "Time", "Payment", "cogs", "gross margin percentage", "gross income", "Rating") 
+FROM 'C:\Users\user\Downloads\data_raw.csv'
+DELIMITER ','
+CSV HEADER;
+
+
+
